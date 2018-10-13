6de82da93c9e6ee3f3af0b6ea4517df401ee11ed
Ray Cao <youdymoo@umich.edu>
2018-10-13 16:49:19 -0400

    localhost

--- server/asciiii/model.py
+++ server/asciiii/model.py
@@ -7,11 +7,11 @@ import asciiii
 
 
 def create_ascii(photo, lines, color):
-    return asciiii.app.config["RESULTS_FOLDER"] + '2.png'
+    contents = "                 \ndhjsjdsiiwjdisids\n"
+    return '2.png', contents
 
 
 def save_file(file):
-    """Save image files into /var/uploads/."""
     # Save POST request's file object to a temp file
     dummy, temp_filename = tempfile.mkstemp()
     file.save(temp_filename)
@@ -19,6 +19,8 @@ def save_file(file):
     # Compute filename
     dummy, suffix = os.path.splitext(file.filename)
     filename_uuid = uuid.uuid4().hex + suffix
+    if suffix not in asciiii.app.config["ALLOWED_EXTENSIONS"]:
+        return ""
     filename = os.path.join(
         asciiii.app.config["UPLOAD_FOLDER"],
         filename_uuid
@@ -27,4 +29,4 @@ def save_file(file):
     # Move temp file to permanent location
     shutil.move(temp_filename, filename)
     asciiii.app.logger.debug("Saved %s", filename)
-    return filename
+    return filename_uuid
--- server/asciiii/views/index.py
+++ server/asciiii/views/index.py
@@ -5,11 +5,15 @@ import asciiii
 @asciiii.app.route('/', methods=['GET', 'POST'])
 def show_index():
     if flask.request.method == 'GET':
-        context = {'ifcontent': False}
+        context = {'ifcontent': False, 'ifdebug': False}
         return flask.render_template("index.html", **context)
     photo = asciiii.model.save_file(flask.request.files['file'])
+    if photo == "":
+        context = {'ifcontent': False, 'ifdebug': True}
+        return flask.render_template("index.html", **context)
     lines = flask.request.form['lines']
     color = flask.request.form['color']
-    res = asciiii.model.create_ascii(photo, lines, color)
-    context = {'ifcontent': True, 'origin': photo, 'ascii': res}
+    res, res_ascii = asciiii.model.create_ascii(photo, lines, color)
+    print(photo, lines, colors)
+    context = {'ifcontent': True, 'ifdebug' : False, 'origin': photo, 'ascii': res_ascii}
     return flask.render_template("index.html", **context)
