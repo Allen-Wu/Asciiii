bb9a12073d84b131f7e0bf693f406226ca02ad1b
Ray Cao <youdymoo@umich.edu>
2018-10-13 10:44:30 -0400

    server starter

---
+++ server/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='asciiii',
+    version='0.1.0',
+    packages=['asciiii'],
+    include_package_data=True,
+    install_requires=[
+        'Flask==0.12.2',
+        'arrow==0.10.0',
+        'sh==1.12.14',
+    ],
+)
