903594988d3e160e42e289ced78316daa11fb27a
Tim <xyshen@umich.edu>
2018-10-13 11:01:46 -0400

    merge hamming dist matching into ascii object

--- ascii.py
+++ ascii.py
@@ -4,20 +4,38 @@ import glob
 import os
 
 class ASCII:
-    ascii_dict = {}
+    _ascii_dict = {}
 
     def __init__(self):
-        self.construct_ascii_dict()
+        self._construct_ascii_dict()
 
     def get_array(self, id):
-        return self.ascii_dict[id]
+        return self._ascii_dict[id]
 
-    def ascii_array(self, fname):
+    def _ascii_array(self, fname):
         im = imageio.imread(fname)
         return 255 - im
 
-    def construct_ascii_dict(self):
+    def _construct_ascii_dict(self):
         for im_path in glob.glob('ascii/*.png'):
-            bitmap = self.ascii_array(im_path)
+            bitmap = self._ascii_array(im_path)
             id = int(os.path.basename(im_path).split('.')[0])
-            self.ascii_dict[id] = bitmap
+            self._ascii_dict[id] = bitmap
+
+    def hamming_match(self, measure_grid, gray_scale=True):
+        id_max = 0
+        distance_min = 1e10
+        for id in self._ascii_dict:
+            distance = hamming_dis(self._ascii_dict[id], gray_scale)
+            if distance < distance_min:
+                distance_min = distance
+                id_max = id
+        return id, distance_min
+
+    def _hamming_dis(a, b, gray_scale=False):
+        if gray_scale == False:
+            xor_array = np.bitwise_xor(a, b)
+            return np.count_nonzero(xor_array)
+        else:
+            # Calculate the sum of gray scale diff
+            return np.absolute(a - b).sum()
